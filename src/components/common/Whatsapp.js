104636143a6b802cac2071d6dced72d886946e72
Uchiha <official.qarni@gmail.com>
2023-06-17 18:02:30 +0500

    Add existing file

---
+++ src/components/common/Whatsapp.js
@@ -0,0 +1,37 @@
+import React,{useState} from "react";
+import ChatPopup from "./ChatPopup"
+const Whastapp = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+  const handleChatClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  return (
+    <>
+    <div className="chat_float">
+      <button onClick={handleChatClick}><i class="fa fa-facebook whatsapp-icon "></i></button>
+      <ChatPopup />
+          <div className="overlay" onClick={handleClose}></div>
+    </div>
+      <div
+        title="Whatsapp"
+        // onClick={handlewhastapp}
+      >
+        <a
+          href="https://wa.me/+923125496244"
+          className="whatsapp_float "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fa fa-whatsapp whatsapp-icon "></i>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Whastapp;
